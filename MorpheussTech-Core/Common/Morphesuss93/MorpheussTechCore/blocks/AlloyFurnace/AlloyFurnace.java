fbb24df37e5ba2c02b3c98335640874321d5c8bf
Morpheuss93 <stefanotavonatti93@gmail.com>
2015-07-24 19:30:07 +0200

    Fixed AlloyFurnace Rotation

--- MorpheussTech-Core/Common/Morphesuss93/MorpheussTechCore/blocks/AlloyFurnace/AlloyFurnace.java
+++ MorpheussTech-Core/Common/Morphesuss93/MorpheussTechCore/blocks/AlloyFurnace/AlloyFurnace.java
@@ -19,6 +19,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
@@ -88,7 +89,8 @@ public class AlloyFurnace extends BlockContainer{
 	@SideOnly(Side.CLIENT)
 	public void onBlockAdded(World world,int x,int y,int z ){
 		super.onBlockAdded(world, x, y, z);
-		this.direction(world,x,y,z);
+		//this.direction(world,x,y,z);
+		this.dir2(world, x, y, z);
 	}
 
 	private void direction(World world, int x, int y, int z) {
@@ -120,7 +122,73 @@ public class AlloyFurnace extends BlockContainer{
 		}
 	}
 	
-	public void onBlockPlaceBy(World world,int x,int y,int z,EntityLivingBase entity,ItemStack itemstack){
+	
+	private void dir2(World p_149930_1_, int p_149930_2_, int p_149930_3_, int p_149930_4_)
+    {
+        if (!p_149930_1_.isRemote)
+        {
+            Block block = p_149930_1_.getBlock(p_149930_2_, p_149930_3_, p_149930_4_ - 1);
+            Block block1 = p_149930_1_.getBlock(p_149930_2_, p_149930_3_, p_149930_4_ + 1);
+            Block block2 = p_149930_1_.getBlock(p_149930_2_ - 1, p_149930_3_, p_149930_4_);
+            Block block3 = p_149930_1_.getBlock(p_149930_2_ + 1, p_149930_3_, p_149930_4_);
+            byte b0 = 3;
+
+            if (block.func_149730_j() && !block1.func_149730_j())
+            {
+                b0 = 3;
+            }
+
+            if (block1.func_149730_j() && !block.func_149730_j())
+            {
+                b0 = 2;
+            }
+
+            if (block2.func_149730_j() && !block3.func_149730_j())
+            {
+                b0 = 5;
+            }
+
+            if (block3.func_149730_j() && !block2.func_149730_j())
+            {
+                b0 = 4;
+            }
+
+            p_149930_1_.setBlockMetadataWithNotify(p_149930_2_, p_149930_3_, p_149930_4_, b0, 2);
+        }
+    }
+	
+	@Override
+	public void onBlockPlacedBy(World p_149689_1_, int p_149689_2_, int p_149689_3_, int p_149689_4_, EntityLivingBase p_149689_5_, ItemStack p_149689_6_)
+    {
+        int l = MathHelper.floor_double((double)(p_149689_5_.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+
+        if (l == 0)
+        {
+            p_149689_1_.setBlockMetadataWithNotify(p_149689_2_, p_149689_3_, p_149689_4_, 2, 2);
+        }
+
+        if (l == 1)
+        {
+            p_149689_1_.setBlockMetadataWithNotify(p_149689_2_, p_149689_3_, p_149689_4_, 5, 2);
+        }
+
+        if (l == 2)
+        {
+            p_149689_1_.setBlockMetadataWithNotify(p_149689_2_, p_149689_3_, p_149689_4_, 3, 2);
+        }
+
+        if (l == 3)
+        {
+            p_149689_1_.setBlockMetadataWithNotify(p_149689_2_, p_149689_3_, p_149689_4_, 4, 2);
+        }
+
+        if (p_149689_6_.hasDisplayName())
+        {
+            ((TileEntityFurnace)p_149689_1_.getTileEntity(p_149689_2_, p_149689_3_, p_149689_4_)).func_145951_a(p_149689_6_.getDisplayName());
+        }
+    }
+	
+	/*public void onBlockPlaceBy(World world,int x,int y,int z,EntityLivingBase entity,ItemStack itemstack){
 		int direction = MathHelper.floor_double((double) (entity.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
 
 		if (direction == 0) {
@@ -144,7 +212,7 @@ public class AlloyFurnace extends BlockContainer{
 		}
 		
 		
-	}
+	}*/
 	
 	public static void updateBlockState(boolean burning,World world,int x,int y,int z){
 		int direction =world.getBlockMetadata(x, y, z);
